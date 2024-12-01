a3d3d028e8c324deafa14ca0d4765f6538bfb813
Pietro Di Bello <pierodibello@gmail.com>
2024-12-01 17:18:52 +0100

    add formatter

--- hello.fsx
+++ hello.fsx
@@ -1,21 +1,14 @@
-let addx x = x + 1
+let add1 x = x + 1
 
-addx 1
-addx 2
+let sum x y = x + y
+let result = add1 2
 
-let add x y = x + y
-
-add 1 2
-add 3 4
-
-// create a function to print the result of the add function
-let printAdd x y = printfn "%d + %d = %d" x y (add x y)
-
-printAdd 1 2
-
-// create a person record
-type Person = { Name: string; Age: int }
-
-let person = { Name = "John"; Age = 30 }
+printfn "%A" result // 3
 
 
+
+type Person = { name: string; age: string }
+
+let person = { name = "John"; age = "30" }
+
+printfn "%A" person
