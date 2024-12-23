2c864c51a8c97da6bae3cf7e56432206e62b40bf
Pietro Di Bello <pierodibello@gmail.com>
2024-12-23 15:32:24 +0000

    modeling the domain in fsharp

--- order-taking.fsx
+++ order-taking.fsx
@@ -1,3 +1,5 @@
+// Domain - "nouns"
+
 type WidgetCode = WidgetCode of string // A wrapper type. Starting with "W" then 4 digits
 type GizmoCode = GizmoCode of string // A wrapper type. Starting with "G" then 3 digits
 
@@ -5,18 +7,44 @@ type ProductCode =
     | Widget of WidgetCode
     | Gizmo of GizmoCode
 
-[<Struct>]
 type UnitQuantity = UnitQuantity of int // A wrapper type. Between 1 and 100
-[<Struct>]
 type KilogramQuantity = KilogramQuantity of decimal // A wrapper type. Between 0.05 and 100.0
 
 type OrderQuantity =
     | Units of UnitQuantity
     | Kilograms of KilogramQuantity
 
+type Undefined = exn
+
+type CustomerInfo = Undefined
+type ShippingAddress = Undefined
+type BillingAddress = Undefined
+type OrderLine = Undefined
+type AmountToBill = Undefined
+
+type Order =
+    { CustomerInfo: CustomerInfo
+      ShippingAddress: ShippingAddress
+      BillingAddress: BillingAddress
+      OrderLines: OrderLine list
+      AmountToBill: AmountToBill }
+
+// Domain - "verbs"
+
+type ValidateOrder = UnvalidatedOrder -> Result<ValidatedOrder, ValidationError list>
+
+and ValidationError =
+    { FieldName: string
+      ErrorDescription: string }
+
+
+
+// examples
+
 let widgetCode = WidgetCode "W1234"
 let gizmoCode = GizmoCode "G123"
 
-let processWidgetCode (WidgetCode widgetCode) = printfn "Processing widget code %s" widgetCode
+let processWidgetCode (WidgetCode widgetCode) =
+    printfn "Processing widget code %s" widgetCode
 
 processWidgetCode widgetCode
