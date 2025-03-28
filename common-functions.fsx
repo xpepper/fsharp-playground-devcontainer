040e4276a58302659334ba8334aa483aa6fd7122
Pietro Di Bello <pierodibello@gmail.com>
2025-03-28 21:13:49 +0000

    refactor Result module: encapsulate bind function and add map function for Result type transformations

--- common-functions.fsx
+++ common-functions.fsx
@@ -1,7 +1,12 @@
-let bind (switchFn: 'a -> Result<'b,'e>) =
-    fun (twoTrackInput: Result<'a,'e>) ->
-        match twoTrackInput with
-        | Ok success -> switchFn success
-        | Error failure -> Error failure
+module Result =
+    let bind switchFn =
+        fun twoTrackInput->
+            match twoTrackInput with
+            | Ok success -> switchFn success
+            | Error failure -> Error failure
 
-
+    let map singleTrackFn =
+        fun twoTrackInput ->
+            match twoTrackInput with
+            | Ok success -> Ok (singleTrackFn success)
+            | Error failure -> Error failure
